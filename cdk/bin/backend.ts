b1a191d37742dd4810404b38ac80eeee61ddf108
Joerg Henning <1712460+joerx@users.noreply.github.com>
2020-11-13 23:23:34 +0800

    Some comments

--- cdk/bin/backend.ts
+++ cdk/bin/backend.ts
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
+
 import "source-map-support/register";
 import * as cdk from "@aws-cdk/core";
-// import { SecretValue } from "@aws-cdk/core";
-// import { BackendCdkPipeline } from "../lib/backend-pipeline";
-import { BackendStack } from "../lib/backend-stack";
 import { SecretValue } from "@aws-cdk/core";
 import { BackendCdkPipeline } from "../lib/backend-pipeline";
 import { GitHubSourceProps } from "../lib/shared";
@@ -20,17 +18,28 @@ const dns = {
   recordName: "api-dev",
 };
 
+// How is branching going to work? Pipeline is deployed only on change to master?
+// What about multiple team members working on the same application? How do they 
+// test their changes locally? 
+// Presumably the deployed changes are staging or pre-prod, so they represent the 
+// integration stage. For local development, engineers use local deployment (e.g.
+// SAM, docker compose, etc.), unit tests, etc. Integration tests are either part
+// of the CDK pipeline or a separate CI pipeline.
+// So the workflow looks roughly like this:
+//
+// - Code, build and test locally
+// - Push changes, create a PR
+// - Automated unit and integration tests
+// - PR review, appproval
+// - Merge to master updates pipeline, deploys preprod
+// - Additional stages, manual approval, etc.
+
 const source: GitHubSourceProps = {
   repo: "lolcatz-backend",
   owner: "joerx",
   oauthToken: SecretValue.secretsManager("github/oauth-token"),
-  branch: "cdk-pipeline",
+  branch: "master",
   subdirectory: "cdk",
 };
-
-// new BackendStack(app, "LolcatzBackend", {
-//   dns,
-//   env,
-// });
 
 new BackendCdkPipeline(app, "LolcatzBackendCdkPipeline", { source, dns, env });
